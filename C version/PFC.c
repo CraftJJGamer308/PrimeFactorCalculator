cc111121839427642602335299217be07a3d3010
임정주 <41723716+CraftJJGamer308@users.noreply.github.com>
2021-06-20 11:12:19 +0200

    Added C file with extremely fast algorithm
    The calculation limit is updated every time the integer is divided. Allows values up to unsigned long long. Compiled with gcc

---
+++ C version/PFC.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <math.h>
+
+int prime(int x)
+{
+    int count = 0;
+    if (x == 2)
+        return 1;
+
+    for (int i = 1; i <= (int)sqrt(x); i++)
+    {
+        if (x % i == 0)
+            count++;
+        if (count >= 2)
+            return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+    unsigned long long x;
+    unsigned long long i = 2;
+    unsigned int exp = 0;
+    unsigned long long lim = floor(sqrt(x));
+
+    printf("Craft JJ Industries Extreme PFC\n");
+    printf("Value supported: unsigned long long [+2, +18,446,744,073,709,551,615]\n");
+
+    do
+    {
+        printf("Enter an integer greater than 1: ");
+        scanf("%llu", &x);
+    } while (x < 2);
+
+    while (1)
+    {
+        if (x == 1)
+            break;
+        if (i >= lim)
+        {
+            printf("%llu^1 * ", x);
+            break;
+        }   
+
+        if (prime(i) == 1)
+        {
+            if (x % i == 0)
+            {
+                while (x % i == 0)
+                {
+                    x /= i;
+                    exp++;
+                }
+                printf("%llu^%u * ", i, exp);
+                exp = 0;
+            }
+            lim = floor(sqrt(x));
+        }
+        if (i == 2)
+            i++;
+        else
+            i += 2;
+    }
+    printf("\b\b");
+    return 0;
+}
